d195ce69c1fd7dabf4cf4fa710a009d4849a1150
Gilberto Murillo <gilmurillo11@gmail.com>
2013-04-29 14:36:53 -0700

    Adding Project Lion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lion::Application.config.secret_token = '9005580a48d7bfd49dc4acf1876325503bede2ca789e78871eacb74969af993e4832f5a93b7d10e63370433b73602497f4877eeaab61716b4c17fbc102f7507f'
